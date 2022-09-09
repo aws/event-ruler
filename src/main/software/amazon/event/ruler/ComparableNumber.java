24aa15c24b97232d09d541627920931d352091e3
Rudy Alvarez <rudygt@gmail.com>
2022-09-09 18:21:38 +0200

    Replace String.format with faster toHexString (#37)
    * replace String.format with faster custom decimal to hex method
    * make it easier to read
    
    The benefits are more obvious when you run a custom test case (see conversation in pull request https://github.com/aws/event-ruler/pull/37)

--- src/main/software/amazon/event/ruler/ComparableNumber.java
+++ src/main/software/amazon/event/ruler/ComparableNumber.java
@@ -1,6 +1,7 @@
 package software.amazon.event.ruler;
 
-import java.util.Locale;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Represents a number, turned into a comparable string
@@ -9,7 +10,7 @@ import java.util.Locale;
  *  They are all treated as floating point
  *  They are turned into strings by:
  *  1. Add 10**9 (so no negatives), then multiply by 10**6 to remove the decimal point
- *  2. Format with the format string %014x" because hex string converted from 5e9*1e6=10e15 has 14 characters.
+ *  2. Format to a 14 char string left padded with 0 because hex string converted from 5e9*1e6=10e15 has 14 characters.
  *  Note: We use Hex because of a) it can save 3 bytes memory per number than decimal b) it aligned IP address radix.
  *  If needed, we can consider to use 32 or 64 radix description to save more memory,e.g. the string length will be 10
  *  for 32 radix, and 9 for 64 radix.
@@ -31,15 +32,62 @@ import java.util.Locale;
 class ComparableNumber {
     private static final double TEN_E_SIX = 1E6;
     static final int MAX_LENGTH_IN_BYTES = 16;
+    private static final String HEXES = new String(Constants.HEX_DIGITS, StandardCharsets.US_ASCII);
+    public static final int NIBBLE_SIZE = 4;
 
-    private ComparableNumber() { }
+    // 1111 0000
+    private static final int UPPER_NIBBLE_MASK = 0xF0;
+
+    // 0000 1111
+    private static final int LOWER_NIBBLE_MASK = 0x0F;
+
+    private ComparableNumber() {
+    }
 
     static String generate(final double f) {
         if (f < -Constants.FIVE_BILLION || f > Constants.FIVE_BILLION) {
             throw new IllegalArgumentException("Value must be between " + -Constants.FIVE_BILLION +
                     " and " + Constants.FIVE_BILLION + ", inclusive");
         }
-        return String.format("%014x", (long) (TEN_E_SIX * (Constants.FIVE_BILLION + f))).toUpperCase(Locale.ROOT);
+        return toHexStringSkippingFirstByte((long) (TEN_E_SIX * (Constants.FIVE_BILLION + f)));
     }
+
+    /**
+     * converts a single byte to its two hexadecimal character representation
+     * @param value the byte we want to convert to hex string
+     * @return a 2 digit char array with the equivalent hex representation
+     */
+    static char[] byteToHexChars(byte value) {
+
+        char[] result = new char[2];
+
+        int upperNibbleIndex = (value & UPPER_NIBBLE_MASK) >> NIBBLE_SIZE;
+        int lowerNibbleIndex = value & LOWER_NIBBLE_MASK;
+
+        result[0] = HEXES.charAt(upperNibbleIndex);
+        result[1] = HEXES.charAt(lowerNibbleIndex);
+
+        return result;
+
+    }
+
+    private static byte[] longToByteBuffer(long value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(value);
+        return buffer.array();
+    }
+
+    static String toHexStringSkippingFirstByte(Long value) {
+        byte[] raw = longToByteBuffer(value);
+        char[] outputChars = new char[14];
+        for (int i = 1; i < raw.length; i++) {
+            int pos = (i - 1) * 2;
+            char[] currentByteChars = byteToHexChars(raw[i]);
+            outputChars[pos] = currentByteChars[0];
+            outputChars[pos + 1] = currentByteChars[1];
+        }
+        return new String(outputChars);
+    }
+
 }
 
