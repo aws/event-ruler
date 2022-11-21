1963c8a5b822c9d83c583a43dfee713d6ac0aeaa
Aliaksei Bialiauski <Hialexbel@gmail.com>
2022-11-21 16:18:21 +0300

    refactored tests for parsers in input pkg;

--- src/main/software/amazon/event/ruler/input/StringValueParser.java
+++ src/main/software/amazon/event/ruler/input/StringValueParser.java
@@ -4,7 +4,6 @@ import software.amazon.event.ruler.MatchType;
 
 /**
  * @author Aliaksei Bialiauski (abialiauski@solvd.com)
- * @since 1.0
  */
 @SuppressWarnings("UseOfConcreteClass")
 public interface StringValueParser extends MatchTypeParser {
