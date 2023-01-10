dfdfa71bb14ff08902a0ee5d5e71d547f08ea7b7
Rudy Alvarez <rudygt@gmail.com>
2023-01-10 16:10:18 +0100

    use fastdoubleparser to improve double parsing speed (#54)
    Using FastDoubleParser improves convering to double. We do not see full benefits yet because of inherent issues within the subsequent code but this was a relatively small change to improve eventual performance. 
    
    No new dependencies were added as we're reusing code that Jackson uses under the cover. Also the reason we feel comfortable with changing this API.

--- src/main/software/amazon/event/ruler/ByteMachine.java
+++ src/main/software/amazon/event/ruler/ByteMachine.java
@@ -1,5 +1,6 @@
 package software.amazon.event.ruler;
 
+import com.fasterxml.jackson.core.io.doubleparser.FastDoubleParser;
 import software.amazon.event.ruler.input.InputByte;
 import software.amazon.event.ruler.input.InputCharacter;
 import software.amazon.event.ruler.input.InputCharacterType;
@@ -85,7 +86,7 @@ class ByteMachine {
         boolean fieldValueIsNumeric = false;
         if (hasNumeric.get() > 0) {
             try {
-                final double numerically = Double.parseDouble(valString);
+                final double numerically = FastDoubleParser.parseDouble(valString);
                 valString = ComparableNumber.generate(numerically);
                 fieldValueIsNumeric = true;
             } catch (Exception e) {
