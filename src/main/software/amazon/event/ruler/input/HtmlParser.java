d5dfbb5f700ffc837c8679b2f85bdad858dcc00e
ajithkeerikkattil <akeerikkattil@tistatech.com>
2023-08-23 10:09:34 -0400

    adding custom html parser

--- src/main/software/amazon/event/ruler/input/HtmlParser.java
+++ src/main/software/amazon/event/ruler/input/HtmlParser.java
@@ -12,6 +12,11 @@ public class HtmlParser {
 
     }
 
+    /**
+     * WIP:: Function responsible for parsing the value from the custom HTML rule
+     * @param value: The html value of the rule
+     * @return
+     */
     public InputCharacter[] parse(String value){
         List<InputCharacter> result = new ArrayList<>(value.length());
         return result.toArray(new InputCharacter[0]);
