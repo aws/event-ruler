b9c53aea7a2f6321aae2eca4dc7fc1719fda8a4c
Rishi Baldawa <baldawar@amazon.com>
2024-09-04 13:38:36 -0700

    Remover unused import

--- src/test/software/amazon/event/ruler/ByteMachineTest.java
+++ src/test/software/amazon/event/ruler/ByteMachineTest.java
@@ -3,7 +3,6 @@ package software.amazon.event.ruler;
 import org.junit.Test;
 import software.amazon.event.ruler.input.ParseException;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
